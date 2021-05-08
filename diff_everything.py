5a8fb23dbed708e066579205b23b191da618dc54
Libre Liu <libreliu@foxmail.com>
2021-05-08 02:08:29 +0000

    working diff script

--- diff_everything.py
+++ diff_everything.py
@@ -12,12 +12,21 @@ os.chdir(rightdir)
 all_file_right = glob.glob("*.*")
 os.chdir("../")
 
+error = False
 for fname in all_file_left:
     if fname not in all_file_right:
         print(f"[ERR] {fname} unmatched on the right!")
+        error = True
 
 for fname in all_file_right:
     if fname not in all_file_left:
         print(f"[ERR] {fname} unmatched on the left!")
+        error = True
 
-print("TODO: diff")
+if error:
+    sys.exit(1)
+
+for fname in all_file_left:
+    os.system(f"diff  {leftdir}/{fname} {rightdir}/{fname}")
+
+
